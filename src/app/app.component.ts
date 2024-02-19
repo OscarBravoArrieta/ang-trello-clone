cbdcc642906c8c5f8f7a2687256c006048e55ab5
oscarbravo <oscarbravoarrieta@gmail.com>
2024-02-19 08:36:09 -0500

    Solving error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
  import { Component } from '@angular/core'
  import { CommonModule } from '@angular/common'
  import { RouterOutlet } from '@angular/router'
- import {OverlayModule} from '@angular/cdk/overlay'
+
 
  @Component({
      selector: 'app-root',
      standalone: true,
-     imports: [CommonModule, RouterOutlet, OverlayModule],
+     imports: [CommonModule, RouterOutlet],
      templateUrl: './app.component.html',
      styleUrl: './app.component.scss'
  })
